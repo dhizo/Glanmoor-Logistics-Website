bfe1b2baa3e6dc63c6116214313eb7a66a0d9817
dhizo <tanakatoga54@gmail.com>
2021-06-09 13:06:00 +0200

    first commit

---
+++ contact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+	
+	
+			<h1 style="text-align:center">
+               <b><u> Books Fantastics</u></b>
+            </h1>
+         </div>
+	     <center><img src="images/Book Fantastics-122.jfif" alt="" width="800" height="400" ></center><br><br>
+		 
+   
+	<a href=" https://chatwith.io/s/5ff2b447e1985" style="display: table; font-family: sans-serif; text-decoration: none; margin: 1em auto; color: #fff; font-size: 0.9em; padding: 1em 2em 1em 3.5em; border-radius: 2em; font-weight: bold; background: #25d366 url('https://tochat.be/click-to-chat-direc...) no-repeat 1.5em center; background-size: 1.6em;">
+
+		Click Here To Purchase Book
+		</a>
+    
+
+ 
+
+</body>
+
+</html>
